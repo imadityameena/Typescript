3fe1f7cda8d5634c96a8ba769c3c748cdf416a96
Aditya Meena <adityameena2233@gmail.com>
2025-07-11 13:00:58 +0530

    ts

---
+++ 02basics/functions.js
@@ -0,0 +1,6 @@
+function addTwo(num) {
+    return num + 2;
+}
+addTwo(5);
+var isLoggedIn = function (name, email, isPaid) { };
+isLoggedIn("huppy", "huppy@tmai.com", true);
---
+++ 02basics/functions.ts
@@ -0,0 +1,14 @@
+function addTwo(num: number){
+  return num + 2
+}
+
+addTwo(5);
+
+
+let isLoggedIn = (name: string, email:string, isPaid:boolean) => {
+  
+}
+
+isLoggedIn("huppy", "huppy@tmai.com", true);
+
+export{}
