cdf2626690f6a6e845df4e9f15833f5d4eb4f203
Aditya Meena <adityameena2233@gmail.com>
2025-07-12 18:08:52 +0530

    ts array

---
+++ 02basics/myArray.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeros = [];
+console.log(superHeros);
+// const heroPower : number[] = []
+var heroPower = [];
+var allUsers = [];
+var MLModels = [
+    [255, 255, 255],
+    []
+];
+superHeros.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "huppy", isActive: true });
---
+++ 02basics/myArray.ts
@@ -0,0 +1,35 @@
+const superHeros : string[] = []
+
+
+
+console.log(superHeros);
+
+// const heroPower : number[] = []
+
+const heroPower: Array<number> = []
+
+type User = {
+  name: string
+  isActive : boolean
+}
+
+const allUsers: User[] = []
+
+
+const MLModels : number[][] = [
+  [255,255,255],
+  []
+]
+
+
+
+
+
+
+
+superHeros.push("spiderman")
+heroPower.push(2)
+
+allUsers.push({name:"huppy", isActive: true})
+
+export{}
